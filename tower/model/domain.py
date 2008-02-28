e0484b62835f561695254f6d8993c1c16348a37e
nyergler <nyergler@committer.creativecommons.org>
2008-02-28 22:25:19 +0000

    Split model into individual files for cleanliness.

---
+++ tower/model/domain.py
@@ -0,0 +1,55 @@
+import os
+import babel.messages.pofile
+
+from pylons import config
+
+from jToolkit import prefs
+
+import language
+
+class Domain(object):
+    """A translation domain."""
+
+    _IGNORE_DIRS = ['.svn', 'test', 'templates',]
+
+    @classmethod
+    def by_name(cls, name):
+        """Return a Domain instance by name."""
+
+        if os.path.exists(os.path.join(config.get('tower.po_dir'), name)):
+            return Domain(name, os.path.join(config.get('tower.po_dir'), name))
+
+        raise KeyError("Unknown domain name.")
+
+    @classmethod
+    def all(cls):
+        """Return a sequence of all available domains."""
+
+        return [Domain(n, os.path.join(config.get('tower.po_dir'), n)) 
+                for n in os.listdir(config.get('tower.po_dir'))
+                if n not in cls._IGNORE_DIRS and 
+                   os.path.isdir(os.path.join(config.get('tower.po_dir'), n))]
+
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+    
+    @property
+    def languages(self):
+        """Return a sequence of available languages."""
+
+        return [language.Language(self, n) 
+                for n in os.listdir(self.path)
+                if n not in self._IGNORE_DIRS and
+                   os.path.isdir(os.path.join(self.path, n))]
+
+    def get_language(self, lang):
+        """Return a specific language for this domain."""
+
+        if os.path.exists(os.path.join(self.path, lang)) and \
+                os.path.isdir(os.path.join(self.path, lang)):
+
+            return language.Language(self, lang)
+
+        raise KeyError("Unknown language.")
+
---
+++ tower/model/language.py
@@ -0,0 +1,72 @@
+import os
+import babel.messages.pofile
+
+from pylons import config
+
+from jToolkit import prefs
+
+import domain
+
+class Language(object):
+    """A specific language within a domain."""
+
+    @classmethod
+    def by_domain_id(cls, domain_id, lang):
+
+        return domain.Domain.by_name(domain_id).get_language(lang)
+
+    def __init__(self, domain, lang):
+        self.domain = domain
+        self.lang = lang
+
+    def __str__(self):
+        return self.lang
+
+    def __cmp__(self, other):
+        if isinstance(other, Language):
+            return cmp(str(self), str(other))
+
+        return cmp(self, other)
+
+    @property
+    def name(self):
+        return str(self)
+
+    @property
+    def _po_filename(self):
+
+        return os.path.join(
+            self.domain.path, self.lang, '%s.po' % self.domain.name)
+
+    @property
+    def catalog(self):
+        """Return a sequence of strings for this language."""
+        
+        catalog = babel.messages.pofile.read_po(file(self._po_filename, 'r'),
+                                                locale=self.name,
+                                                domain=self.domain.name)
+
+        return catalog
+
+    @property
+    def prefs(self):
+        """Return the Pootle (sigh) properties object."""
+
+        return prefs.PrefsParser(os.path.join(self.domain.path, self.lang,
+                                             'pootle-%s-%s.prefs' % (
+                    self.domain.name, self.name)))
+
+    def update(self, string_id, new_value, old_value=None):
+        """Update a string; if old_value is provided, only perform the edit
+        if the value has not been editted in the interim."""
+
+        catalog = self.catalog
+        record = catalog[string_id]
+
+        if old_value is not None:
+            if record.string != old_value:
+                raise Exception
+
+        record.string = new_value
+        babel.messages.pofile.write_po(file(self._po_filename, 'w'),
+                                       catalog)
