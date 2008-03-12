bc77b6b3582cd3ebf034bf529c827471787db330
nyergler <nyergler@committer.creativecommons.org>
2008-03-12 23:42:37 +0000

    Include user and role information in the context.

--- tower/lib/base.py
+++ tower/lib/base.py
@@ -10,19 +10,53 @@ from pylons.decorators import jsonify, validate
 from pylons.i18n import _, ungettext, N_
 from pylons.templating import render
 
+from tower.lib.decorators import with_user_info
+
 import tower.lib.helpers as h
 import tower.model as model
 
 class BaseController(WSGIController):
 
-    def _get_roles(self):
-        return []
+    def _get_roles(self, environ):
+        """Return a list of roles for the current context."""
 
+        authkit = environ.get('authkit.users')
+        user = environ.get('REMOTE_USER', None)
+        
+        if user is None:
+            # not logged in, no roles
+            return []
+
+        return authkit.user_roles(user)
+
+    def _actions(self, environ):
+        """Return a sequence of two-tuples describing the actions for this
+        view (taking into account the logged in user, roles, etc)."""
+
+        actions = [
+            ('/domain/all/list', 'Translation Domains'),
+            ]
+
+        if 'administrator' in self._get_roles(environ):
+            actions.append( ('/admin', 'Administration') )
+
+        return actions
+
+    @with_user_info
     def __call__(self, environ, start_response):
         """Invoke the Controller"""
 
-        # inject some stuff into c 
-        c.remote_user = request.environ.get("REMOTE_USER", False)
+        # bind the actions method into the context
+        c.roles = self._get_roles(environ)
+        c.actions = self._actions(environ)
+
+        # add actions
+        if c.remote_user:
+            c.actions.insert(0, ('/account/profile', c.remote_user))
+            c.actions.append( ('/account/logout', 'Logout') )
+        else:
+            c.actions.append( ('/account/login', 'Login') )
+
 
         # WSGIController.__call__ dispatches to the Controller method
         # the request is routed to. This routing information is
